f5e96981d1b991f1b382af589b4274f06313c0cd
Yuheng Zhang <zhangyuheng@users.noreply.github.com>
2018-01-03 17:18:09 +0800

    feat: add getDataListByProjectId (#2)
    * feat: add getDataListByProjectId
    * test: add basic test cases

--- test/datahub-nodejs-sdk.js
+++ test/datahub-nodejs-sdk.js
@@ -1,10 +1,95 @@
 'use strict';
 
-const SDK = require('..');
+const sinon = require('sinon');
 const assert = require('assert');
 
-describe('test', () => {
-  it('should be ok', () => {
+const SDK = require('..');
+
+describe('test', function() {
+
+  let stub;
+
+  it('default options', function() {
     assert.ok(SDK);
   });
+
+  it('pass options', function() {
+    const client = new SDK();
+    assert.deepStrictEqual(client.options, {
+      port: 5678,
+      hostname: '127.0.0.1'
+    });
+  });
+
+  it('updateSceneByProjectIdAndDataId', function() {
+    const client = new SDK();
+    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+      stub.restore();
+      return Promise.resolve(args);
+    });
+    return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
+      currentScene: 'default',
+      delay: '2'
+    }).then(data => {
+      assert(data[0] === 'http://127.0.0.1:5678/api/data/projectId/dataId');
+      assert.deepStrictEqual(data[1], {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: 'currentScene=default&delay=2'
+      });
+    });
+  });
+
+  it('updateSceneByProjectIdAndDataId with null', function() {
+    const client = new SDK();
+    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+      stub.restore();
+      return Promise.resolve(args);
+    });
+    return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
+      currentScene: 'default',
+      delay: null
+    }).then(data => {
+      assert(data[0] === 'http://127.0.0.1:5678/api/data/projectId/dataId');
+      assert.deepStrictEqual(data[1], {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: 'currentScene=default&delay='
+      });
+    });
+  });
+
+  it('getDataListByProjectId success', function() {
+    const client = new SDK();
+    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+      stub.restore();
+      return Promise.resolve({
+        json: () => { return args; }
+      });
+    });
+    return client.getDataListByProjectId('projectId')
+      .then(data => {
+        assert.equal(data[0], 'http://127.0.0.1:5678/api/data/projectId');
+        assert.deepStrictEqual(data[1], {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+      });
+  });
+
+  it('getDataListByProjectId failed', function() {
+    const client = new SDK();
+    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+      stub.restore();
+      return Promise.reject(new Error('fail'));
+    });
+    return client.getDataListByProjectId('projectId')
+      .then(data => {
+        assert.deepStrictEqual(data, {});
+      });
+  });
 });
+
