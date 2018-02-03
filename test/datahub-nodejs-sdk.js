614e052fe07d074a7696a907d4e420fac5cbbe89
Yuheng Zhang <zhangyuheng@users.noreply.github.com>
2018-02-03 18:28:06 +0800

    add api for status and headers (#3)
    * feat: support status and headers
    
    * test: use nyc

--- test/datahub-nodejs-sdk.js
+++ test/datahub-nodejs-sdk.js
@@ -5,96 +5,214 @@ const assert = require('assert');
 
 const SDK = require('..');
 
-describe('test', function() {
-
-  let stub;
-
-  it('default options', function() {
+describe('sdk test', function () {
+  it('default options', function () {
     assert.ok(SDK);
   });
 
-  it('pass options', function() {
+  it('pass options', function () {
     const client = new SDK();
     assert.deepStrictEqual(client.options, {
       port: 5678,
-      hostname: '127.0.0.1'
+      hostname: '127.0.0.1',
     });
   });
 
-  it('updateSceneByProjectIdAndDataId', function() {
+  it('updateSceneByProjectIdAndDataId', function () {
     const client = new SDK();
-    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
-      stub.restore();
-      return Promise.resolve(args);
+    const stub1 = sinon.stub(client, 'getDataByProjectIdAndDataId').callsFake(function (...args) {
+      stub1.restore();
+      return Promise.resolve({
+        success: true,
+        data: {
+          proxyContent: '{}',
+        },
+      });
+    });
+    const stub2 = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub2.restore();
+      return Promise.resolve({
+        json: () => Promise.resolve(args),
+      });
     });
     return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
       currentScene: 'default',
-      delay: '2'
+      delay: '2',
     }).then(data => {
       assert(data[0] === 'http://127.0.0.1:5678/api/data/projectId/dataId');
       assert.deepStrictEqual(data[1], {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: 'currentScene=default&delay=2'
+        body: 'currentScene=default&delay=2&proxyContent=%7B%7D',
       });
     });
   });
 
-  it('updateSceneByProjectIdAndDataId with null', function() {
+  it('updateSceneByProjectIdAndDataId with null', function () {
     const client = new SDK();
-    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
-      stub.restore();
-      return Promise.resolve(args);
+    const stub1 = sinon.stub(client, 'getDataByProjectIdAndDataId').callsFake(function (...args) {
+      stub1.restore();
+      return Promise.resolve({
+        success: true,
+        data: {
+          proxyContent: '{}',
+        },
+      });
+    });
+    const stub2 = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub2.restore();
+      return Promise.resolve({
+        json: () => Promise.resolve(args),
+      });
     });
     return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
       currentScene: 'default',
-      delay: null
+      delay: null,
     }).then(data => {
       assert(data[0] === 'http://127.0.0.1:5678/api/data/projectId/dataId');
       assert.deepStrictEqual(data[1], {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: 'currentScene=default&delay='
+        body: 'currentScene=default&delay=&proxyContent=%7B%7D',
       });
     });
   });
 
-  it('getDataListByProjectId success', function() {
+  it('updateSceneByProjectIdAndDataId getData failed', function () {
     const client = new SDK();
-    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+    const stub = sinon.stub(client, 'getDataByProjectIdAndDataId').callsFake(function (...args) {
       stub.restore();
       return Promise.resolve({
-        json: () => { return args; }
+        success: false,
+        data: {},
+      });
+    });
+    return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
+      currentScene: 'default',
+      delay: null,
+    }).then(data => {
+      assert.deepStrictEqual(data, {
+        success: false,
+        data: {},
+      });
+    });
+  });
+
+  it('updateSceneByProjectIdAndDataId proxyContent parse failed', function () {
+    const client = new SDK();
+    const stub = sinon.stub(client, 'getDataByProjectIdAndDataId').callsFake(function (...args) {
+      stub.restore();
+      return Promise.resolve({
+        success: true,
+        data: {
+          proxyContent: 'invalid json string',
+        },
+      });
+    });
+    return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
+      currentScene: 'default',
+      delay: null,
+    }).then(data => {
+      assert.deepStrictEqual(data, {
+        success: false,
+        data: {},
+      });
+    });
+  });
+
+  it('updateSceneByProjectIdAndDataId update failed', function () {
+    const client = new SDK();
+    const stub1 = sinon.stub(client, 'getDataByProjectIdAndDataId').callsFake(function (...args) {
+      stub1.restore();
+      return Promise.resolve({
+        success: true,
+        data: {
+          proxyContent: '{}',
+        },
+      });
+    });
+    const stub2 = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub2.restore();
+      return Promise.reject(new Error('fail'));
+    });
+    return client.updateSceneByProjectIdAndDataId('projectId', 'dataId', {
+      currentScene: 'default',
+      delay: null,
+    }).then(data => {
+      assert.deepStrictEqual(data, {
+        success: false,
+        data: {},
+      });
+    });
+  });
+
+  it('getDataListByProjectId success', function () {
+    const client = new SDK();
+    const stub = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub.restore();
+      return Promise.resolve({
+        json: () => { return Promise.resolve(args); },
       });
     });
     return client.getDataListByProjectId('projectId')
       .then(data => {
         assert.equal(data[0], 'http://127.0.0.1:5678/api/data/projectId');
         assert.deepStrictEqual(data[1], {
-          method: 'GET',
           credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
         });
       });
   });
 
-  it('getDataListByProjectId failed', function() {
+  it('getDataListByProjectId failed', function () {
     const client = new SDK();
-    stub = sinon.stub(client, 'fetch').callsFake(function(...args) {
+    const stub = sinon.stub(client, 'fetch').callsFake(function (...args) {
       stub.restore();
       return Promise.reject(new Error('fail'));
     });
     return client.getDataListByProjectId('projectId')
       .then(data => {
-        assert.deepStrictEqual(data, {});
+        assert.deepStrictEqual(data, {
+          success: false,
+          data: {},
+        });
+      });
+  });
+
+  it('getDataByProjectIdAndDataId success', function () {
+    const client = new SDK();
+    const stub = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub.restore();
+      return Promise.resolve({
+        json: () => { return Promise.resolve(args); },
+      });
+    });
+    return client.getDataByProjectIdAndDataId('projectId', 'dataId')
+      .then(data => {
+        assert.equal(data[0], 'http://127.0.0.1:5678/api/data/projectId/dataId');
+        assert.deepStrictEqual(data[1], {
+          credentials: 'same-origin',
+        });
+      });
+  });
+
+  it('getDataByProjectIdAndDataId failed', function () {
+    const client = new SDK();
+    const stub = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub.restore();
+      return Promise.reject(new Error('fail'));
+    });
+    return client.getDataByProjectIdAndDataId('projectId', 'dataId')
+      .then(data => {
+        assert.deepStrictEqual(data, {
+          success: false,
+          data: {},
+        });
       });
   });
 });
---
+++ test/fetch.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const sinon = require('sinon');
+const assert = require('assert');
+
+const SDK = require('..');
+
+describe('fetch test', function () {
+  it('get with params', function () {
+    const client = new SDK();
+    const stub = sinon.stub(client, 'fetch').callsFake(function (...args) {
+      stub.restore();
+      return Promise.resolve({
+        json: () => Promise.resolve(args),
+      });
+    });
+    return client.get('data/baz/fred', { key: 'value' }, {
+      headers: {
+        'proxy-key': 'proxy-value',
+      },
+    }).then(data => {
+      assert(data[0] === 'http://127.0.0.1:5678/api/data/baz/fred?key=value');
+      assert.deepStrictEqual(data[1], {
+        credentials: 'same-origin',
+        headers: {
+          'proxy-key': 'proxy-value',
+        },
+      });
+    });
+  });
+});
+
